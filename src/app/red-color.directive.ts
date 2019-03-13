5a6328d0acaae981f93a4d5e33e13d578fbebfd8
Manishthakur1297 <Manishthakur1297@gmail.com>
2019-03-13 10:15:05 +0530

    z

--- src/app/red-color.directive.ts
+++ src/app/red-color.directive.ts
@@ -1,13 +1,38 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[redColor]'
 })
 export class RedColorDirective {
 
+  i=1;
   constructor(element : ElementRef) 
   {
       element.nativeElement.style.color="red";
+  }
+
+  @HostListener('click')
+  abc()
+  {
+    console.log("Clicked......"+this.i++)
+  }
+
+  @HostListener('mouseenter')
+  xyz()
+  {
+    console.log("Entered......"+this.i++)
+  }
+
+  @HostListener('mouseleave')
+  atoz()
+  {
+    console.log("Leaving......"+this.i++)
+  }
+
+  @HostListener('mousemove')
+  xqw()
+  {
+    console.log("Moving......"+this.i++)
   }
 
 }
